a5d3b683b8a74fa938304a96bb53215883da6882
Sadat15 <sadatmanjur15@gmail.com>
2023-01-14 01:39:10 +0000

    Added gitignore file

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
+import API from "./config";
 import axios from "axios";
 
 const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
-      Authorization: "Client-ID VKX-3Vu6EWNwg3FMUK44KCpYjqg69ZkqeLfxL2kartQ",
+      Authorization: `Client-ID ${API}`,
     },
     params: {
       query: term,
